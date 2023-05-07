e7bcf731d1947c5e131ff3f45d509ff089322c70
jorgegallegou <jorgegallego.u@gmail.com>
2023-05-06 21:52:47 -0500

    Creación de la estructura de archivos utilizando Node.js+Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
