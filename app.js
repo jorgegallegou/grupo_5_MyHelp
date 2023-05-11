7c4185c24c09b827dc4d339f53e71598aa463037
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-05-10 21:59:29 -0500

    agregados todos los wirefarme pendiente las descripciones de Santiago Alvarez y Tatiana Pitta

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const path = require('path')
-
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"))
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
-})
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
