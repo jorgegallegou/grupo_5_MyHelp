55d2d802ab0f0c44e3f1cdee68702fd5338e242f
jorgegallegou <jorgegallego.u@gmail.com>
2023-05-17 17:31:11 -0500

    git co

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/productCart.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+
