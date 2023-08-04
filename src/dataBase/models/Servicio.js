82772adbb929c96dfd36c2253a315a60cee5f306
jorgegallegou <jorgegallego.u@gmail.com>
2023-08-03 20:01:13 -0500

    Instalado y configurado sequelize. Creado el modelo Servicio

---
+++ src/dataBase/models/Servicio.js
@@ -0,0 +1,48 @@
+module.exports = (Sequelize, DataTypes) => {
+    const alias = "Servicio";
+    const cols = {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      precio: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      descripcion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      categoria: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imagen: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id_categorias_servicios: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATETIME
+      },
+      updated_at: {
+        type: DataTypes.DATETIME
+      },
+    };
+    const config = {
+      tableName: "servicios",
+      timestamps: false,
+    };
+    const servicio = Sequelize.define(alias, cols, config);
+  
+    return servicio;
+  };
