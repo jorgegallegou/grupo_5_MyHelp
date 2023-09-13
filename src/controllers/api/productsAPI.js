1143f41a964dcb4ecc08f26f22523afa382a88ad
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-09-13 15:43:43 -0500

    api de productos funcionando dando cumplimiento segun consignas sprint 8

--- src/controllers/api/productsAPI.js
+++ src/controllers/api/productsAPI.js
@@ -1,31 +1,34 @@
-const { response } = require('express');
 const db = require('../../dataBase/models');
+const { Op } = require('sequelize');
+const Servicio = db.Servicio;
+const CategoriaServicio = db.CategoriaServicio;
 
 module.exports = {
   productListHome: async (req, res) => {
     try {
-      const clean = await db.Servicio.findAll({
-        where: {
-          id_categorias_servicios: 1,
-          deleted_at: null,
-        },
+      const response = {};
+
+      const [allHomeServices, categorias] = await Promise.all([
+        Servicio.findAll(
+          { where: { id_categorias_servicios: [1, 2] } },
+          { include: [{ association: 'categorias' }] }
+        ),
+        CategoriaServicio.findAll({ include: [{ association: 'servicios' }] }),
+      ]);
+      response.count = allHomeServices.length;
+      response.countByCategory = {};
+      categorias.forEach((row) => {
+        response.countByCategory[row.name] = row.servicios.length;
       });
-      const special = await db.Servicio.findAll({
-        where: {
-          id_categorias_servicios: 2,
-          deleted_at: null,
-        },
+      response.products = allHomeServices.map((row) => {
+        return {
+          id: row.id,
+          name: row.nombre,
+          description: row.descripcion,
+          categories: row.categorias,
+          detail: `/api/products/${row.id}`,
+        };
       });
-      const response = {
-        meta: {
-          success: true,
-          endPoint: '/api/products/productCompany',
-        },
-        data: {
-          clean: clean,
-          special: special,
-        },
-      };
       return res.json(response);
     } catch (error) {
       console.log(error);
@@ -33,28 +36,30 @@ module.exports = {
   },
   productListCompany: async (req, res) => {
     try {
-      const cleaning = await db.Servicio.findAll({
-        where: {
-          id_categorias_servicios: 3,
-          deleted_at: null,
-        },
+      const response = {};
+
+      const [allCompanyServices, categorias] = await Promise.all([
+        Servicio.findAll(
+          { where: { id_categorias_servicios: [3, 4] } },
+          { include: [{ association: 'categorias' }] }
+        ),
+        CategoriaServicio.findAll({ include: [{ association: 'servicios' }] }),
+      ]);
+
+      response.count = allCompanyServices.length;
+      response.countByCategory = {};
+      categorias.forEach((row) => {
+        response.countByCategory[row.name] = row.servicios.length;
       });
-      const disinfection = await db.Servicio.findAll({
-        where: {
-          id_categorias_servicios: 4,
-          deleted_at: null,
-        },
+      response.products = allCompanyServices.map((row) => {
+        return {
+          id: row.id,
+          name: row.nombre,
+          description: row.descripcion,
+          categories: row.categorias,
+          detail: `/api/products/${row.id}`,
+        };
       });
-      const response = {
-        meta: {
-          success: true,
-          endPoint: '/api/products/productListCompany',
-        },
-        data: {
-          cleaning: cleaning,
-          disinfection: disinfection,
-        },
-      };
       return res.json(response);
     } catch (error) {
       console.log(error);
@@ -62,25 +67,18 @@ module.exports = {
   },
   productDetailId: async (req, res) => {
     try {
+      const response = {};
       const serviceDetail = await db.Servicio.findByPk(req.params.id, {
-        where: {
-          deleted_at: null,
-        },
-        include: [
-          {
-            association: 'categorias',
-          },
-        ],
+        where: { deleted_at: null },
+        include: [{ association: 'categorias' }],
       });
-      const response = {
-        meta: {
-          success: true,
-          endPoint: `/api/products/productDetail/${req.params.id}`,
-        },
-        data: {
-          serviceDetail: serviceDetail,
-        },
+      response.meta = {
+        success: true,
+        endPoint: `/api/products/productDetail/${req.params.id}`,
       };
+      response.data = serviceDetail;
+      response.data.imagen = `/public/img/imgServiciosMyHelp/${serviceDetail.imagen}`;
+
       return res.json(response);
     } catch (error) {}
   },
@@ -105,7 +103,7 @@ module.exports = {
   },
   productLoad: async (req, res) => {
     try {
-      let categorias = await db.CategoriaServicio.findAll();
+      let categorias = await CategoriaServicio.findAll();
       const response = {
         meta: {
           success: true,
