6a6a52a1d8cd4617936768e18bfee095dbee547f
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-10-23 08:45:55 -0500

    before changes

---
+++ src/controllers/api/mainAPI.js
@@ -0,0 +1,8 @@
+const db = require('../../dataBase/models');
+const { Op } = require('sequelize');
+const Servicio = db.Servicio;
+const CategoriaServicio = db.CategoriaServicio;
+
+module.exports = {
+  servicesInRanking: (req, res) => {},
+};
